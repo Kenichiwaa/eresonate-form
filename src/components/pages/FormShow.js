6d186bfd55b97ee03aaaae8e995f55d9a2fc7c1f
Ken Sugai <kenichisugai@gmail.com>
2020-07-01 22:04:22 -0700

    build form page

--- src/components/pages/FormShow.js
+++ src/components/pages/FormShow.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createForm } from "../../actions";
 
-const FormShow = () => {
-  return <div>FormShow</div>;
+class FormShow extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    console.log(meta);
+    console.log(meta.touched);
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.createForm(formValues);
+  };
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a valid title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a valid description";
+  }
+  return errors;
 };
 
-export default FormShow;
+const formWrapped = reduxForm({
+  form: "FormShow",
+  validate,
+})(FormShow);
+
+export default connect(null, { createForm })(formWrapped);
