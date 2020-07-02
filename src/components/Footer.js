87976315eb7d6964cd3a1a380dc87f7a354eba4f
Ken Sugai <kenichisugai@gmail.com>
2020-07-02 01:37:44 -0700

    add header and footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  height: 194px;
+  background-color: #292c32;
+  padding: 50px 0;
+  margin-top: 50px;
+`;
+
+const SocialIconsContainer = styled.div`
+  max-width: 350px;
+  margin: auto;
+  text-align: center;
+
+  i {
+    margin: 0 40px;
+    color: #5c99f8;
+  }
+`;
+
+const LinkContainer = styled.div`
+  &.ui.grid {
+    margin: 20px 0;
+  }
+
+  .link {
+    opacity: 0.6;
+    font-family: Rubik;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    letter-spacing: normal;
+    text-align: center;
+    color: white;
+    margin: auto;
+    padding-bottom: 1em;
+  }
+`;
 
 const Footer = () => {
   return (
-    <div class="ui segment">
-      <h3 class="ui center aligned header">Footer</h3>
-    </div>
+    <FooterContainer className="ui">
+      <SocialIconsContainer>
+        <i className="twitter icon large" />
+        <i className="linkedin icon large" />
+        <i className="instagram icon large" />
+      </SocialIconsContainer>
+      <LinkContainer className="ui equal width grid">
+        <div className="column">
+          <div className="ui link">Claim Your Venue</div>
+          <div className="ui link">Venue Log In</div>
+        </div>
+        <div className="column">
+          <div className="ui link">Terms and Condition</div>
+          <div className="ui link">Privacy Policy</div>
+        </div>
+        <div className="column">
+          <div className="ui link button">Default</div>
+        </div>
+      </LinkContainer>
+    </FooterContainer>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  margin-bottom: 0;
+  background-color: #292c32;
+  color: white;
+  height: 80px;
+  text-align: center;
+`;
+
+const StyledIcon = styled.i`
+  padding-top: 10px;
+  color: #367cf7;
+  right: -15px;
+  position: relative;
+`;
+
+const StyledSearchIcon = styled.i`
+  &.big.icon {
+    float: right;
+    top: 28px;
+    position: relative;
+    right: 10%;
+  }
+`;
 
 const Header = () => {
+  const onSearchClick = () => {
+    console.log("Search Clicked");
+  };
   return (
-    <div class="ui segment">
-      <h3 class="ui center aligned header">Header</h3>
-    </div>
+    <HeaderContainer>
+      <StyledIcon className=" icon huge circle" />
+      <StyledSearchIcon className="icon search big" onClick={onSearchClick} />
+    </HeaderContainer>
   );
 };
 
